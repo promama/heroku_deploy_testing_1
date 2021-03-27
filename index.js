c621e6ab9b8bed8f18b14705bc5ba0cbc2a78612
promama <promama@users.noreply.github.com>
2021-03-27 22:47:18 +0700

    create hello world
    create hello world project and save on git

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', function(req, res) {
+    res.send("hello world")
+})
+
+app.listen(port, function(err) {
+    if (err) {
+        console.log("smth when wrong")
+    }
+    console.log("server listening to port: " + port)
+})
